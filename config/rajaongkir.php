b19371e3a856e75642dfaf8b238a5c61d3d1ee9e
makruva <30585474+makruva@users.noreply.github.com>
2018-02-15 17:27:03 +0700

    Create rajaongkir.php

---
+++ config/rajaongkir.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * RajaOngkir API Key
+ * Silakan daftar akun di RajaOngkir.com untuk mendapatkan API Key
+ * http://rajaongkir.com/akun/daftar
+ */
+$config['rajaongkir_api_key'] = "";
